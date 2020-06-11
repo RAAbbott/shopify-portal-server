cb3b6c17a7888aac8f4940e6ff3245f00383fd20
Alex Abbott <Robert.Abbott@henryschein.com>
2020-06-11 11:49:45 -0600

    Few more changes to add tags and such

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// require('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const crypto = require('crypto');
@@ -11,7 +11,7 @@ const querystring = require('querystring');
 const request = require('request-promise');
 const apiKey = process.env.SHOPIFY_API_KEY;
 const apiSecret = process.env.SHOPIFY_API_SECRET_KEY;
-const scopes = 'read_orders,read_products,read_customers';
+const scopes = 'read_orders,read_products,read_customers,write_orders';
 const forwardingAddress = "https://eom-shopify-server.herokuapp.com"; // Replace this with your HTTPS Forwarding address (Server Address)
 const clientAddress = "https://xenodochial-curie-ca34f4.netlify.app/" //Client Address
 const Shopify = require('shopify-api-node');
@@ -21,7 +21,9 @@ let shopify;
 
 const bodyParser = require('body-parser');
 const corsOptions = {
-    origin: 'http://localhost:3000.com',
+    origin: 'http://localhost:3000',
+    methods: "POST,GET,OPTIONS",
+    allowedHeaders: "Content-Type,Content-Length, Authorization, Accept,X-Requested-With",
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 
@@ -119,23 +121,45 @@ app.get('/shopify', (req, res) => {
   });
 
 app.get('/orders', async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
-    // res.setHeader('Access-Control-Allow-Credentials', true); // If needed
-    res.json(await formatOrders());
+  shopify = new Shopify({
+    shopName: process.env.SHOP_NAME,
+    apiKey: process.env.API_KEY,
+    password: process.env.PASSWORD
+  })
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
+  // res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+  res.json(await formatOrders());
 });
 
 app.get('/', (req, res) => {
     res.redirect(clientAddress);
 })
 
+app.post('/completeOrders', async (req, res) => {
+  console.log(req.body.orderIds);
+  if (!shopify) {
+    shopify = new Shopify({
+      shopName: process.env.SHOP_NAME,
+      apiKey: process.env.API_KEY,
+      password: process.env.PASSWORD
+    })
+  }
+
+  const tag = 'EOM-READY';
+  const orders = req.body.orderIds;
+  orders.forEach(orderId => {
+    shopify.order.update(orderId, {tags: 'EOM-READY'}).then(resp => console.log(resp)).catch(err => console.log(err));
+  });
+})
+
 // Helper Methods
 
 async function getOrders() {
     if (shopify) {
-        return await shopify.order.list({limit: 100, status: 'open', fulfillment: 'open'}).then(orders => orders).catch(err => err);
+        return await shopify.order.list({limit: 150, status: 'open', fulfillment: 'open'}).then(orders => orders).catch(err => err);
     }
   }
 
@@ -178,8 +202,10 @@ async function getOrders() {
         customerEmail: order.email || 'CUSTOMER NOT FOUND',
         amount: order.total_price,
         note: order.note,
-        dateOrdered: moment(order.updated_at).format('MMM DD'),
-        products: productIds
+        dateOrdered: moment(order.created_at).format('MMM DD'),
+        products: productIds,
+        completed: false,
+        tags: order.tags
       })
     });
 
@@ -187,110 +213,3 @@ async function getOrders() {
     return {orderList, productList};
 
   }
-
-// const dotenv = require('dotenv').config();
-// const express = require('express');
-// const app = express();
-// const crypto = require('crypto');
-// const cookie = require('cookie');
-// const nonce = require('nonce')();
-// const querystring = require('querystring');
-// const request = require('request-promise');
-
-// const apiKey = process.env.SHOPIFY_API_KEY;
-// const apiSecret = process.env.SHOPIFY_API_SECRET_KEY;
-// const scopes = 'read_orders,read_products,read_customers';
-// const forwardingAddress = "https://b35edd7f.ngrok.io"; // Replace this with your HTTPS Forwarding address
-// const Shopify = require('shopify-api-node');
-// let shopify;
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-
-// app.get('/shopify', (req, res) => {
-//     console.log('hit!!!');
-
-//     const shop = req.query.shop;
-//     if (shop) {
-//       const state = nonce();
-//       const redirectUri = forwardingAddress + '/shopify/callback';
-//       const installUrl = 'https://' + shop +
-//         '/admin/oauth/authorize?client_id=' + apiKey +
-//         '&scope=' + scopes +
-//         '&state=' + state +
-//         '&redirect_uri=' + redirectUri;
-  
-//       res.cookie('state', state);
-//       res.redirect(installUrl);
-//     } else {
-//       return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
-//     }
-//   });
-
-//   app.get('/shopify/callback', (req, res) => {
-//     const { shop, hmac, code, state } = req.query;
-//     const stateCookie = cookie.parse(req.headers.cookie).state;
-  
-//     if (state !== stateCookie) {
-//       return res.status(403).send('Request origin cannot be verified');
-//     }
-  
-//     if (shop && hmac && code) {
-//       // DONE: Validate request is from Shopify
-//       const map = Object.assign({}, req.query);
-//       delete map['signature'];
-//       delete map['hmac'];
-//       const message = querystring.stringify(map);
-//       const providedHmac = Buffer.from(hmac, 'utf-8');
-//       const generatedHash = Buffer.from(
-//         crypto
-//           .createHmac('sha256', apiSecret)
-//           .update(message)
-//           .digest('hex'),
-//           'utf-8'
-//         );
-//       let hashEquals = false;
-  
-//       try {
-//         hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
-//       } catch (e) {
-//         hashEquals = false;
-//       };
-  
-//       if (!hashEquals) {
-//         return res.status(400).send('HMAC validation failed');
-//       }
-  
-//       // DONE: Exchange temporary code for a permanent access token
-//       const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
-//       const accessTokenPayload = {
-//         client_id: apiKey,
-//         client_secret: apiSecret,
-//         code,
-//       };
-  
-//       request.post(accessTokenRequestUrl, { json: accessTokenPayload })
-//       .then(async (accessTokenResponse) => {
-//         const accessToken = accessTokenResponse.access_token;
-//         // DONE: Use access token to make API call to 'shop' endpoint
-//         shopify = new Shopify({
-//             shopName: shop,
-//             accessToken
-//         });
-//         console.log(shopify);
-//         console.log(await shopify.order.list({limit: 5}).then(orders => orders).catch(err => err.response.body));
-//         res.redirect('/');
-//       })
-//       .catch((error) => {
-//         res.status(error.statusCode).send(error.error.error_description);
-//       });
-  
-//     } else {
-//       res.status(400).send('Required parameters missing');
-//     }
-//   });
-
-// app.listen(3060, () => {
-//   console.log('Example app listening on port 3060!');
-// });
