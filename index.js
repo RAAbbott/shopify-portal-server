53d9d8ded031181a2bbbb0ac94be009238ce7b06
Alex Abbott <Robert.Abbott@henryschein.com>
2020-06-21 20:23:38 -0600

    Application is pretty much complete, I'll be branching off and creating a demo version that only uses mock data.

--- index.js
+++ index.js
@@ -139,7 +139,26 @@ app.get('/', (req, res) => {
 })
 
 app.post('/completeOrders', async (req, res) => {
-  console.log(req.body.orderIds);
+  const tag = 'EOM-READY';
+  const orders = req.body.orderIds;
+  let error = false;
+  const asyncForEach = async (array, callback) => {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array);
+    }
+  }
+  const updateTags = async () => {
+    await asyncForEach(orders, async (orderId) => {
+      try {
+        await shopify.order.update(orderId, {tags: 'EOM-READY'});
+        console.log('success');
+      } catch (error) {
+        console.log(error);
+        error = true;
+      }
+    })
+  }
+
   if (!shopify) {
     shopify = new Shopify({
       shopName: process.env.SHOP_NAME,
@@ -148,11 +167,14 @@ app.post('/completeOrders', async (req, res) => {
     })
   }
 
-  const tag = 'EOM-READY';
-  const orders = req.body.orderIds;
-  orders.forEach(orderId => {
-    shopify.order.update(orderId, {tags: 'EOM-READY'}).then(resp => console.log(resp)).catch(err => console.log(err));
-  });
+  await updateTags();
+
+  if (error) {
+    res.status(500).send("Error When Updating Orders, Please Try Again");
+  } else {
+    res.status(200).send("All Orders Successfully Updated");
+  }
+  
 })
 
 // Helper Methods
@@ -205,6 +227,7 @@ async function getOrders() {
         dateOrdered: moment(order.created_at).format('MMM DD'),
         products: productIds,
         completed: false,
+        marked: false,
         tags: order.tags
       })
     });
